42a5cb34ef5100d6e12580bac4afa0e4de03e07f
John Angarita <jnangaritaortiz@gmail.com>
2024-05-21 15:48:03 -0500

    feat: Add features to login page
    - Added new styles and features to the login page.
    
    Refs: TRELLO: #7

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -9,49 +9,78 @@
     </section>
     <section class="login-data">
       <h1 class="title">{{ $t("message.welcome") }}</h1>
-      <form action="">
+      <form @submit.prevent="login">
         <fieldset>
           <input
             :placeholder="$t('message.email')"
             class="display-flex input-login-page"
-            type="text"
+            id="email-login"
+            required
+            type="email"
+            v-model="loginData.email"
           />
           <input
             :placeholder="$t('message.password')"
             class="display-flex input-login-page"
+            id="password-login"
+            pattern="[a-z0-5]{8,}"
+            required
             type="password"
+            v-model="loginData.password"
           />
-          <label class="display-flex" for="remember">
-            <input id="remember" type="checkbox" />{{
-              $t("message.rememberMe")
-            }}
+          <label class="display-flex" for="remember-data">
+            <input
+              id="remember-data"
+              name="remember-data"
+              required
+              type="checkbox"
+              v-model="loginData.rememberData"
+            />{{ $t("message.rememberMe") }}
           </label>
           <input class="btn-login" type="submit" :value="$t('message.login')" />
         </fieldset>
         <hr />
-        <fieldset>
-          <button class="btn-login btn-google">
-            {{ $t("message.signInWithGoogle") }}
-          </button>
-          <button class="btn-login btn-facebook">
-            {{ $t("message.signInWithFacebook") }}
-          </button>
-        </fieldset>
-        <hr />
-        <fieldset>
-          <p>
-            <a href="">{{ $t("message.forgetPassword") }}</a>
-          </p>
-          <p>
-            <a href="">{{ $t("message.createAccount") }}</a>
-          </p>
-        </fieldset>
       </form>
+      <div>
+        <button class="btn-login btn-google" @click="login">
+          {{ $t("message.signInWithGoogle") }}
+        </button>
+        <button class="btn-login btn-facebook" @click="login">
+          {{ $t("message.signInWithFacebook") }}
+        </button>
+        <hr />
+      </div>
+      <div>
+        <p>
+          <a @click.prevent="goHome">{{ $t("message.forgetPassword") }}</a>
+        </p>
+        <p>
+          <a @click.prevent="goHome">{{ $t("message.createAccount") }}</a>
+        </p>
+      </div>
     </section>
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const loginData = ref({
+  email: "",
+  password: "",
+  rememberData: false,
+});
+
+const login = () => {
+  console.log(loginData.value);
+  router.push("/");
+};
+
+const goHome = () => {
+  router.push("/");
+};
 </script>
 <style scoped>
 .login-container {
